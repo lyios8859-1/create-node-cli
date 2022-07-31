40389463f4d798467e5da50a9227e76056773023
七月十五 <194962218@qq.com>
2022-07-31 10:25:47 +0800

    :tada:Alerts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Nodejs CLI
 
-> 使用具有命令行的 Node.js 工具 `npx yun-node`
+> 使用具有命令行的 Node.js 工具 `npx yun-node`.
 
 ## 使用方式
 
--- index.js
+++ index.js
@@ -5,9 +5,14 @@
  * setp2: npm link 到全局
  * setp3: 执行 package.json 中的 bin 字段中的命令, 这里设置 bin: {"yun": "src/index.js"}, 就可以执行 `yun` 命令
  */
+const chalk = require("chalk");
+
 const welcome = require("./utils/cli-welcome");
+const alerts = require("./utils/cli-alerts");
 
 const pkgJson = require("./package.json");
+
+const { bio, ad, social, blog, blogName } = require("./utils/data");
 
 welcome({
   title: pkgJson.name,
@@ -20,4 +25,33 @@ welcome({
   clear: true,
 });
 
-console.log("Node CLI - Test");
+const log = console.log;
+
+log(`${chalk.dim(bio)}
+${social}
+${chalk.dim(blogName + ": " + blog)}
+
+${chalk.blue(ad)}`);
+
+alerts({
+  type: "success",
+  msg: "success.",
+  name: "",
+});
+alerts({
+  type: "warning",
+  msg: "warning.",
+  name: "",
+});
+alerts({
+  type: "info",
+  msg: "info.",
+  name: "",
+});
+alerts({
+  type: "error",
+  msg: "error.",
+  name: "",
+});
+
+log("\nNode CLI - Test\n");
