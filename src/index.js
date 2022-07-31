1b7a06735986e02927cf3cd480bda182fb17bb4a
七月十五 <194962218@qq.com>
2022-07-31 09:18:16 +0800

    :sparkles: Welcome

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@
  * setp2: npm link 到全局
  * setp3: 执行 package.json 中的 bin 字段中的命令, 这里设置 bin: {"yun": "src/index.js"}, 就可以执行 `yun` 命令
  */
-
-const chalk = require("chalk");
 const clearConsole = require("./utils/clear-any-console");
+const welcome = require("./cli/cli-welcome");
 
-// 清空控制台
-clearConsole();
+const pkgJson = require("../package.json");
+
+welcome({
+  title: pkgJson.name,
+  tagLine: "Hello, nice to meet you!",
+  description: pkgJson.description,
+  version: pkgJson.version,
+  bgColor: "#FADC00",
+  color: "#000000",
+  bold: true,
+  clear: true,
+});
 
 console.log("Node CLI - Test");
